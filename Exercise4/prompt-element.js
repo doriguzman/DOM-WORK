f9f629e3050efeec1c9c819bb74fc18716737b1c
doriguzman <dorianguzman@ac.c4q.nyc>
2017-10-30 17:33:29 -0400

    newnew

--- Exercise4/prompt-element.js
+++ Exercise4/prompt-element.js
@@ -5,8 +5,13 @@ let text=prompt("write some text")
 let ending=prompt("write a closing tag")
 
 document.getElementById("container").innerHTML=opening + text+ ending
-// document.getElementById("container").innerText=text
-// document.getElementById("container").innerHTML=ending
 
 }
 print()
+
+
+// let element = window.prompt("Write an HTML element.");
+
+// let container = document.getElementById('container');
+
+// container.innerHTML = element;
--- Exercise6/swap-list-items.js
+++ Exercise6/swap-list-items.js
@@ -3,10 +3,12 @@ let initial2= prompt("Again, please pick one of the four choice: one, two, three
 
 
 
+let x = document.getElementById(initial).innerText
+let y = document.getElementById(initial2).innerText
 
 
-document.getElementById(initial).innerHTML= document.getElementById(initial2).innerHTML
 
-document.getElementById(initial2).innerHTML= document.getElementById(initial).innerHTML
+document.getElementById(initial).innerHTML= y
+document.getElementById(initial2).innerHTML=x
 
 
