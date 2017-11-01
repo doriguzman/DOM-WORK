7fed2ae4f431c0372f34f0776256b39c48a950e3
doriguzman <dorianguzman@ac.c4q.nyc>
2017-11-01 11:35:05 -0400

    edited

--- EVENTS/clicker/clicker.js
+++ EVENTS/clicker/clicker.js
@@ -1,10 +1,26 @@
        
-        document.addEventListener('click', function(event){
-            let width= window.innerWidth
-            if ((width - event.clientX)/ width > .5){
-                document.body.style.backgroundColor= 'mediumaquamarine'
-            }else{
-                document.body.style.backgroundColor='peachpuff'
-            }
-        })
-    
+        // document.addEventListener('click', function(event){
+        //     let width= window.innerWidth
+        //     if ((width - event.clientX)/ width > .5){
+        //         document.body.style.backgroundColor= 'mediumaquamarine'
+        //     }else{
+        //         document.body.style.backgroundColor='peachpuff'
+        //     }
+        // })
+    var body = document.querySelector('body')
+    body.addEventListener('click', function(event){
+        if (event.clientX<window.innerWidth/2){
+            body.style.backgroundColor='mediumaquamarine';
+        }else{
+            body.style.backgroundColor='peachpuff'
+        }
+    })
+
+
+//~~~~~~short cut is using a ternary operator ~~~~~~
+
+// var body = documentquerySelctor('body')
+// document.addEventListener('click', function(event){
+// var isLeftSide=event.clientX<window.innerWidth/2;
+// body.style.backgroundColor=isLeftSide ? 'mediumaquamarine' : 'peachpuff'
+// })
--- EVENTS/echo-er/echo-er.js
+++ EVENTS/echo-er/echo-er.js
@@ -1,11 +1,20 @@
-document.addEventListener('keypress', function(event){
-    let key= event.key;
-    let newnew=document.querySelector('h1')
-    newnew.innerText= key
-       })
-   
+document.addEventListener('keypress', function (event) {
+    let key = event.key;
+    let newnew = document.querySelector('h1')
+    newnew.innerText += key
 
-    //    Continuing with the files echo-er.html, echo-er.js, and echo-er.css 
-    //    add a text-decoration: underline property to the h1 element when the user types a 
-    //    capital letter, and remove it when the user types a period, question mark, or exclamation
-    //     point.
+    if (key >= 'A' && key <= 'Z') {
+        newnew.style.textDecoration = 'underline';
+    } else if ('.?!'.includes(key)) {
+        newnew.style.textDecoration = 'none'
+    }
+
+
+    // if (key === key.toUpperCase()) {
+    //     newnew.style.textDecoration= "underline"
+    // } else if (key===key.toLowerCase()|| key==='!'||key==='.'|| key ==='?'){
+    //     newnew.style.textDecoration= "none"
+    // }
+
+
+})
